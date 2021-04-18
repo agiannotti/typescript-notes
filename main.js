0273aa9debff1cc4a51542fa723ae4ad7f9b08f5
tonyg <aj.gianno@gmail.com>
2021-04-18 17:28:33 -0500

    ts assertions

--- main.js
+++ main.js
@@ -10,3 +10,15 @@
 // let d: any;
 // let e: number[] = [1, 2, 3];
 // let f: any[] = [1, true, 'a', false];
+//enum validation
+var ColorRed = 0;
+var ColorGreen = 1;
+var ColorBlue = 2;
+// setting var nums explicitly.
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Green"] = 1] = "Green";
+    Color[Color["Blue"] = 2] = "Blue";
+})(Color || (Color = {}));
+var backgroundColor = Color.Red;
--- main.ts
+++ main.ts
@@ -17,15 +17,25 @@
 
 //enum validation
 
-const ColorRed = 0;
-const ColorGreen = 1;
-const ColorBlue = 2;
+// const ColorRed = 0;
+// const ColorGreen = 1;
+// const ColorBlue = 2;
 
 // setting var nums explicitly.
-enum Color {
-  Red = 0,
-  Green = 1,
-  Blue = 2,
-}
+// enum Color {
+//   Red = 0,
+//   Green = 1,
+//   Blue = 2,
+// }
 
-let backgroundColor = Color.Red;
+// let backgroundColor = Color.Red;
+
+// type assertions
+
+// you can prefix with type
+
+let message;
+message = 'abc';
+let endsWithC = (<string>message).endsWith('c');
+// alternatively
+// let alternativeWay = (message as string).endsWith('c')

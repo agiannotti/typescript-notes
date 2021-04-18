0ec049d6765e4fe8796b5f9b7581e4e3539c6cf6
tonyg <aj.gianno@gmail.com>
2021-04-18 17:20:37 -0500

    enum

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
-var count = 5;
-count = 'a';
+// type annotations
+// let a: number;
+// a=1;
+// a = true;
+// a = '1'
+// variable types
+// let a: number;
+// let b: boolean;
+// let c: string;
+// let d: any;
+// let e: number[] = [1, 2, 3];
+// let f: any[] = [1, true, 'a', false];
--- main.ts
+++ main.ts
@@ -14,3 +14,18 @@
 // let d: any;
 // let e: number[] = [1, 2, 3];
 // let f: any[] = [1, true, 'a', false];
+
+//enum validation
+
+const ColorRed = 0;
+const ColorGreen = 1;
+const ColorBlue = 2;
+
+// setting var nums explicitly.
+enum Color {
+  Red = 0,
+  Green = 1,
+  Blue = 2,
+}
+
+let backgroundColor = Color.Red;

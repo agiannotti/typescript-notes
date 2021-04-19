6d42bf3f84b7bcae54ec28c22dadf667240f4bd5
tonyg <aj.gianno@gmail.com>
2021-04-18 19:48:42 -0500

    set & get class conventions and variable declaration

--- Access-modifiers.ts
+++ Access-modifiers.ts
@@ -2,18 +2,30 @@
 // i.e public, private, protected
 // default is public
 
-class Point {
-  private x: number;
-  y: number;
-  constructor(x?: number, y?: number) {
-    this.x = x;
-    this.y = y;
-  }
+// class Point {
+//   private x: number;
+//   private y: number;
+//   constructor(x?: number, y?: number) {
+//     this.x = x;
+//     this.y = y;
+//   }
 
-  draw() {
-    console.log('x: ' + this.x, 'y: ' + this.x);
-  }
-}
+//   draw() {
+//     console.log('x: ' + this.x, 'y: ' + this.x);
+//   }
+// }
 
-let point = new Point(1, 2);
-point.draw();
+// let point = new Point(1, 2);
+// point.draw();
+
+// // if you prefix a parameter using an access modifier, TS will compile field with same name and value of argument
+// class Point {
+//   constructor(private x?: number, private y?: number) {}
+
+//   draw() {
+//     console.log('x: ' + this.x, 'y: ' + this.x);
+//   }
+// }
+
+// let point = new Point(1, 2);
+// point.draw();
